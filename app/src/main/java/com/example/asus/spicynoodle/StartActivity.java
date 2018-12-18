2391c5880c0b5be5b88def3c8f245a5338865926
dalinvuth1997 <dalinv3@gmail.com>
2018-12-18 09:56:45 +0700

    change location google service.json

---
+++ app/src/main/java/com/example/asus/spicynoodle/StartActivity.java
@@ -0,0 +1,28 @@
+package com.example.asus.spicynoodle;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        new Handler().postDelayed(runnable,1000);
+
+    }
+
+
+
+}
